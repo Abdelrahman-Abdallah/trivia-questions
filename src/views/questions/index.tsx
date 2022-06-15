9569be953021fc0e8ca1caa5ac167b4ce6ceee29
Abdelrahman Abdallah <abdelrahman202022@gmail.com>
2022-06-15 21:50:02 +0200

    add question base component

--- src/views/questions/index.tsx
+++ src/views/questions/index.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Page from "src/components/page";
+import { getQuestions } from "src/lib/questions";
 import { useSelector } from "src/store";
+import { Question } from "src/types/Question";
 
 const Questions = () => {
   const selectedCateogy = useSelector((state) => state.categories.selectedCategorys);
-  const [questions, setQuestions] = useState({});
-  return <div>question</div>;
+  const { level, token } = useSelector((state) => state.user);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const category = selectedCateogy[selectedCateogy.length - 1];
+
+  useEffect(() => {
+    getQuestions(level, token, category).then((res) => {
+      console.log(res);
+      setQuestions(res);
+    });
+  }, [level, token, category]);
+
+  return <Page title="Questions"></Page>;
 };
 
 export default Questions;
